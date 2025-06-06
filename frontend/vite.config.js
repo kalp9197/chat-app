2ee66bf69734df5f7b9078bf048bb281ad07ce7d
kalp9197 <pkalp61@gmail.com>
2025-06-06 18:07:35 +0530

    Hiding Firebase Service Worker Credentials

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,31 @@
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve("./src"),
+export default defineConfig(({ mode }) => {
+  //eslint-disable-next-line
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react(), tailwindcss()],
+    define: {
+      __FIREBASE_API_KEY__: JSON.stringify(env.VITE_FIREBASE_API_KEY),
+      __FIREBASE_AUTH_DOMAIN__: JSON.stringify(env.VITE_FIREBASE_AUTH_DOMAIN),
+      __FIREBASE_PROJECT_ID__: JSON.stringify(env.VITE_FIREBASE_PROJECT_ID),
+      __FIREBASE_STORAGE_BUCKET__: JSON.stringify(
+        env.VITE_FIREBASE_STORAGE_BUCKET
+      ),
+      __FIREBASE_MESSAGING_SENDER_ID__: JSON.stringify(
+        env.VITE_FIREBASE_MESSAGING_SENDER_ID
+      ),
+      __FIREBASE_APP_ID__: JSON.stringify(env.VITE_FIREBASE_APP_ID),
+      __FIREBASE_VAPID_KEY__: JSON.stringify(env.VITE_FIREBASE_VAPID_KEY),
     },
-  },
+    resolve: {
+      alias: {
+        "@": path.resolve("./src"),
+      },
+    },
+  };
 });
